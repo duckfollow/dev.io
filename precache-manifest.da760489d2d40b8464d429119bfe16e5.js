fbbc1cdd9d16acbc0e818193b52270869ab6369f
Tank <prasit.s@8interactive.co.th>
2020-10-16 15:24:35 +0700

    Updates

---
+++ precache-manifest.da760489d2d40b8464d429119bfe16e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad12ab56511cf108aeccf16afd690d42",
+    "url": "/dev.io/index.html"
+  },
+  {
+    "revision": "2f4f6d1fa3d6b90e2948",
+    "url": "/dev.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc12a50bc443d73c9ad2",
+    "url": "/dev.io/static/js/2.69e4faaa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dev.io/static/js/2.69e4faaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f4f6d1fa3d6b90e2948",
+    "url": "/dev.io/static/js/main.a3fb0ff4.chunk.js"
+  },
+  {
+    "revision": "502a69380f4ec46f4581",
+    "url": "/dev.io/static/js/runtime-main.c87e9a05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dev.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
