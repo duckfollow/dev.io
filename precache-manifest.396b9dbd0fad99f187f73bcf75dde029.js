d3b7563b2295af0b659868ba25af8d7db043cfcb
Prasit Supancho <p.supancho@gmail.com>
2020-10-25 19:21:59 +0700

    Updates

---
+++ precache-manifest.396b9dbd0fad99f187f73bcf75dde029.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84b084156f1daea8189fde5000d6396",
+    "url": "/dev.io/index.html"
+  },
+  {
+    "revision": "9161eb0ffa3c0e4c0152",
+    "url": "/dev.io/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d1229e84f1d18f83a5a9",
+    "url": "/dev.io/static/css/main.26e63d1d.chunk.css"
+  },
+  {
+    "revision": "9161eb0ffa3c0e4c0152",
+    "url": "/dev.io/static/js/2.e6180632.chunk.js"
+  },
+  {
+    "revision": "1099a9d51daeb8951751260232a72a15",
+    "url": "/dev.io/static/js/2.e6180632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1229e84f1d18f83a5a9",
+    "url": "/dev.io/static/js/main.30c3fafb.chunk.js"
+  },
+  {
+    "revision": "502a69380f4ec46f4581",
+    "url": "/dev.io/static/js/runtime-main.c87e9a05.js"
+  },
+  {
+    "revision": "06468580303d77d9afd2801a2ca80086",
+    "url": "/dev.io/static/media/facebook.06468580.svg"
+  },
+  {
+    "revision": "42d4d3715e17e6a629cad649dbac1cf2",
+    "url": "/dev.io/static/media/github.42d4d371.svg"
+  },
+  {
+    "revision": "62593ffa4c7bb7261c7f40a15fb164c3",
+    "url": "/dev.io/static/media/instagram.62593ffa.svg"
+  },
+  {
+    "revision": "b9ec4ef9a664d74de5854c06501312f5",
+    "url": "/dev.io/static/media/profile.b9ec4ef9.jpg"
+  },
+  {
+    "revision": "f58833da58c48a42f5c7edfa8891d60f",
+    "url": "/dev.io/static/media/twitter.f58833da.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dev.io/precache-manifest.fe3b6c4638e185e4cd92f769575be8c2.js"
+  "/dev.io/precache-manifest.396b9dbd0fad99f187f73bcf75dde029.js"
 );
 
 self.addEventListener('message', (event) => {
